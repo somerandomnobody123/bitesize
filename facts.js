7f8ab494a5e28f807a3563aee2b4f18ce5b0e6c5
somerandomnobody123 <somerandomnobody123@gmail.com>
2026-05-19 23:28:33 +0800

    added some facts

--- facts.js
+++ facts.js
@@ -1,4 +1,16 @@
 const FACTS = {
+  "2026-05-23": {
+    fact: 'Hippos can easily outpace a human.',
+    detail: 'Hippos can reach land speeds of up to 30mph or 48km/h, while Usain Bolt\'s top speed was around 27.78mph or 44.72km/h.'
+  },
+  "2026-05-22": {
+    fact: 'Over 99% of species that have ever existed on Earth have gone extinct.',
+    detail: 'The average lifespan of a species is between 1 and 10 million years.',
+  },
+  "2026-05-21": {
+    fact: 'The word "Dord" was an accidental fake word printed in the 1934 Webster\'s New International Dictionary, Second Edition.',
+    detail: 'In 1931, a chemistry editor wanted to add "density" as a word that the letter d can abbreviate to, writing it as "D or d", however this was misinterpreted and "Dord" was incorrectly added to the dictionary.',
+  },
   "2026-05-20": {
     fact: "The elevator shaft was invented before the elevator.",
     detail: 'In 1853, Fred A. Petersen ordered a vertical shaft to be made throughout the height of a building, preceding the invention of elevators in 1857, though it was made circular compared to the standard rectangular shape.',
